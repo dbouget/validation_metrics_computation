Fix errors in metrics computation (postop) and new studies

Studies/hgg_postop_segmentation.py
@@ -11,6 +11,7 @@ from Plotting.metric_versus_binned_boxplot import compute_binned_metric_over_met
 import matplotlib.pyplot as plt
 import seaborn as sns
 from pathlib import Path
+from pyCompare import blandAltman
 
 
 class HGGPostopSegmentationStudy:
@@ -38,17 +39,19 @@ class HGGPostopSegmentationStudy:
     def run(self):
         self.__retrieve_optimum_values()
         self.__read_results()
+        self.__drop_exclude_ids()
         self.__compute_and_plot_overall()
         if self.extra_patient_parameters is not None:
-            self.__compute_and_plot_metric_over_metric_categories(data=self.results, metric1='Dice', metric2='Volume', metric2_cutoffs=[1.])
-            volume_figure_fname = Path(self.input_folder, 'Validation', 'volume_cutoff.png')
-            self.__compute_and_plot_volume_cutoff_results(volume_cutoff_range=[0., 0.5], optimal_cutoff=0.175, save_fname=volume_figure_fname)
+            self.__compute_and_plot_metric_over_metric_categories(data=self.results, metric1='Dice', metric2='True postop volume', metric2_cutoffs=[1.])
+            # volume_figure_fname = Path(self.input_folder, 'Validation', 'volume_cutoff.png')
+            # self.__compute_and_plot_volume_cutoff_results(volume_cutoff_range=[0., 0.5], optimal_cutoff=0.175, save_fname=volume_figure_fname)
             results_cutoff = self.__compute_results_cutoff_volume(cutoff_volume=0.175)
             results_cutoff.to_csv(Path(self.input_folder, 'Validation', 'all_dice_scores_volume_cutoff.csv'), index=False)
             compute_fold_average(self.input_folder, results_cutoff, best_threshold=self.optimal_threshold,
                                  best_overlap=self.optimal_overlap,
                                  suffix='volume_cutoff', output_folder=str(Path(self.input_folder, 'Validation')))
-
+            results_cutoff = self.__compute_EOR(results_cutoff, crop_to_zero=True)
+            self.__study_volume_and_EOR(results_cutoff)
 
     def __retrieve_optimum_values(self):
         study_filename = os.path.join(self.input_folder, 'Validation', 'optimal_dice_study.csv')
@@ -77,6 +80,13 @@ class HGGPostopSegmentationStudy:
         except Exception as e:
             print('{}'.format(traceback.format_exc()))
 
+    def __drop_exclude_ids(self):
+        drop_index_res = self.results[self.results['Patient'].isin(self.exclude_ids)].index
+        self.results.drop(drop_index_res, axis=0, inplace=True)
+
+        drop_index_opt = self.optimal_results[self.optimal_results['Patient'].isin(self.exclude_ids)].index
+        self.optimal_results.drop(drop_index_opt, axis=0, inplace=True)
+
     def __compute_and_plot_overall(self):
         """
         Generate average results across all folds and per fold for all the computed metrics.
@@ -97,7 +107,7 @@ class HGGPostopSegmentationStudy:
             self.__compute_dice_confidence_intervals(data=results_df)
 
             if self.extra_patient_parameters is not None:
-                self.__compute_results_metric_over_metric(data=results_df, metric1='Dice', metric2='Volume', suffix='')
+                self.__compute_results_metric_over_metric(data=results_df, metric1='Dice', metric2='True postop volume', suffix='')
         except Exception as e:
             print('{}'.format(traceback.format_exc()))
 
@@ -229,7 +239,7 @@ class HGGPostopSegmentationStudy:
                         results[k].append(v)
 
                 output = f"Cutoff = {cutoff}, recall = {res['Recall']}, precision = {res['Precision']}, "\
-                         f"F1 = {res['F1']}, accuracy = {res['Accuracy']}, tnr = {res['True negative rate']}, "\
+                         f"F1 = {res['F1']}, accuracy = {res['Accuracy']}, tnr = {res['Specificity']}, "\
                          f"positive rate = {res['Positive rate']}, negative rate = {res['Negative rate']}"
                 print(output)
 
@@ -248,45 +258,101 @@ class HGGPostopSegmentationStudy:
 
     def __compute_results_cutoff_volume(self, cutoff_volume, cutoff_gt=True):
         data = deepcopy(self.optimal_results)
-        data.loc[(data['Volume segmentation'] <= cutoff_volume), 'Volume segmentation'] = 0.0
-        data.loc[(data['Volume segmentation'] == cutoff_volume), '#Det'] = 0
+        data.loc[(data['Predicted postop volume'] <= cutoff_volume), 'Predicted postop volume'] = 0.0
+        data.loc[(data['Predicted postop volume'] == cutoff_volume), '#Det'] = 0
 
         if cutoff_gt:
-            data.loc[(data['Volume'] <= cutoff_volume), 'Volume'] = 0.0
-            data.loc[(data['Volume'] == 0), '#GT'] = 0
+            data.loc[(data['True postop volume'] <= cutoff_volume), 'True postop volume'] = 0.0
+            data.loc[(data['True postop volume'] == 0), '#GT'] = 0
 
         # Drop columns missing volume
-        data.dropna(axis=0, subset=['Volume'], inplace=True)
+        data.dropna(axis=0, subset=['True postop volume'], inplace=True)
 
         return data
+
+    def __compute_EOR(self, results, crop_to_zero=False):
+        data = deepcopy(results)
+
+        true_EOR = np.array((results.loc[:, 'True preop volume'] - results.loc[:, 'True postop volume']) / results.loc[:, 'True preop volume'])
+        data['True EOR'] = true_EOR
+        #print(true_EOR[np.where(true_EOR<0)])
+        #print(data.loc[np.where(true_EOR < 0), 'Patient'].values)
+
+        predicted_EOR_type1 = np.array((results.loc[:, 'True preop volume'] - results.loc[:, 'Predicted postop volume']) / results.loc[:, 'True preop volume'])
+        data['Predicted EOR type 1'] = predicted_EOR_type1
+        #print(predicted_EOR_type1[np.where(predicted_EOR_type1 < 0)])
+        #print(data.loc[np.where(predicted_EOR_type1 < 0), 'Patient'].values)
+
+        if crop_to_zero:
+            data.loc[(data['True EOR'] < 0), 'True EOR'] = 0.0
+            data.loc[(data['Predicted EOR type 1'] < 0), 'Predicted EOR type 1'] = 0.0
+
+        return data
+
+    def __study_volume_and_EOR(self, results):
+        data = deepcopy(results)
+        output_folder = Path(self.input_folder, 'Validation', 'Volume-EOR')
+        output_folder.mkdir(exist_ok=True)
+        sns.set_style('ticks')
+
+        # EOR
+        save_fname = str(Path(output_folder, f'EOR_scatter_{self.study_name}_Validation.png'))
+        plt.figure()
+        ax = sns.scatterplot(data=data*100, x='True EOR', y='Predicted EOR type 1')
+        ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c=".1")
+        ax.set(title=f'True vs predicted EOR',
+               xlabel='True EOR (%)', ylabel='Predicted EOR (%)')
+        plt.savefig(save_fname)
+
+        save_fname = str(Path(output_folder, f'EOR_bland_altman_{self.study_name}_Validation.png'))
+        blandAltman(data['True EOR'], data['Predicted EOR type 1'],
+                    title=f'Bland-Altman of true vs predicted EOR',
+                    savePath=save_fname)
+
+        # VOLUME
+        save_fname = str(Path(output_folder, f'volume_scatter_{self.study_name}_Validation.png'))
+        plt.figure()
+        plt.xscale('symlog')
+        plt.yscale('symlog')
+        ax = sns.scatterplot(data=data, x='True postop volume', y='Predicted postop volume')
+        ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c=".1")
+        #ax.title('True vs predicted postop volume (symlog scale)')
+        ax.set(title=f'True vs predicted postop volume (symlog scale)',
+               xlabel='True postop volume (ml)', ylabel='Predicted postop volume (ml)')
+        plt.savefig(save_fname)
+
+        save_fname = str(Path(output_folder, f'volume_bland_altman_{self.study_name}_Validation.png'))
+        blandAltman(data['True postop volume'], data['Predicted postop volume'],
+                    title=f'Bland-Altman of true vs predicted postop volume',
+                    savePath=save_fname)
 
 
 def threshold_volume_and_compute_classification_metrics(optimal_results, cutoff_seg_vol, cutoff_true_vol=0.01):
 
     true_positives = len(
-        optimal_results.loc[(optimal_results['Volume segmentation'] > cutoff_seg_vol) & (optimal_results['Volume'] > cutoff_true_vol)])
+        optimal_results.loc[(optimal_results['Predicted postop volume'] > cutoff_seg_vol) & (optimal_results['True postop volume'] > cutoff_true_vol)])
     false_negatives = len(
-        optimal_results.loc[(optimal_results['Volume segmentation'] <= cutoff_seg_vol) & (optimal_results['Volume'] > cutoff_true_vol)])
+        optimal_results.loc[(optimal_results['Predicted postop volume'] <= cutoff_seg_vol) & (optimal_results['True postop volume'] > cutoff_true_vol)])
     false_positives = len(
-        optimal_results.loc[(optimal_results['Volume segmentation'] > cutoff_seg_vol) & (optimal_results['Volume'] <= cutoff_true_vol)])
+        optimal_results.loc[(optimal_results['Predicted postop volume'] > cutoff_seg_vol) & (optimal_results['True postop volume'] <= cutoff_true_vol)])
     true_negatives = len(
-        optimal_results.loc[(optimal_results['Volume segmentation'] <= cutoff_seg_vol) & (optimal_results['Volume'] <= cutoff_true_vol)])
+        optimal_results.loc[(optimal_results['Predicted postop volume'] <= cutoff_seg_vol) & (optimal_results['True postop volume'] <= cutoff_true_vol)])
     total = true_positives + false_negatives + false_positives + true_negatives
 
     # print(true_positives, false_negatives, false_positives, true_negatives, total)
     # print((true_positives +false_negatives) / total)
     # print((true_negatives + false_positives) / total)
-    dice_pos = np.mean(optimal_results.loc[(optimal_results['Volume'] > cutoff_true_vol), 'Dice'])
-    dice_true_pos = np.mean(optimal_results.loc[(optimal_results['Volume segmentation'] > cutoff_seg_vol) & \
-                                                (optimal_results['Volume'] > cutoff_true_vol), 'Dice'])
+    dice_pos = np.mean(optimal_results.loc[(optimal_results['True postop volume'] > cutoff_true_vol), 'Dice'])
+    dice_true_pos = np.mean(optimal_results.loc[(optimal_results['Predicted postop volume'] > cutoff_seg_vol) & \
+                                                (optimal_results['True postop volume'] > cutoff_true_vol), 'Dice'])
 
     results = {}
     results['Recall'] = true_positives / (true_positives + false_negatives)
     results['Precision'] = true_positives / (true_positives + false_positives)
     results['F1'] = 2 * ((results['Recall'] * results['Precision']) / (results['Precision'] + results['Recall']))
     results['Accuracy'] = (true_positives + true_negatives) / (true_negatives + true_positives + false_negatives + false_positives)
-    results['True negative rate'] = true_negatives / (true_negatives + false_negatives)
-    results['Balanced accuracy'] = (results['Recall'] + results['True negative rate']) / 2
+    results['Specificity'] = true_negatives / (true_negatives + false_positives)
+    results['Balanced accuracy'] = (results['Recall'] + results['Specificity']) / 2
     results['Positive rate'] = (true_positives + false_negatives) / total
     results['Negative rate'] = (true_negatives + false_positives) / total
     results['Dice Positive'] = dice_pos

Studies/hgg_postop_testset_inference.py
@@ -20,6 +20,7 @@ from Validation.validation_utilities import compute_fold_average
 from Validation.extra_metrics_computation import compute_extra_metrics, compute_overall_metrics_correlation
 from Validation.kfold_model_validation import separate_dice_computation
 from Studies.hgg_postop_segmentation import threshold_volume_and_compute_classification_metrics, plot_classification_metrics_volume_cutoffs
+from pyCompare import blandAltman
 
 
 class HGGPostopTestsetInference:
@@ -28,7 +29,7 @@ class HGGPostopTestsetInference:
     """
     def __init__(self):
         self.data_root = SharedResources.getInstance().data_root
-        #self.study_name = SharedResources.getInstance().studies_study_name
+        self.study_name = SharedResources.getInstance().studies_study_name
 
         self.input_folder = SharedResources.getInstance().test_input_folder
         self.base_output_folder = SharedResources.getInstance().test_output_folder
@@ -45,14 +46,23 @@ class HGGPostopTestsetInference:
         print("Detection overlap: ", self.detection_overlap_thresholds)
         self.gt_files_suffix = SharedResources.getInstance().test_gt_files_suffix
         self.prediction_files_suffix = SharedResources.getInstance().test_prediction_files_suffix
+        self.exclude_ids = SharedResources.getInstance().test_exclude_ids
+        if len(self.exclude_ids) > 0:
+            try:
+                exclude_ids = [int(i) for i in self.exclude_ids]
+                self.exclude_ids = exclude_ids
+            except Exception as e:
+                print(f"Convert patient IDs to int failed, error {e}")
 
         self.extra_patient_parameters = None
         if os.path.exists(SharedResources.getInstance().studies_extra_parameters_filename):
             self.extra_patient_parameters = pd.read_csv(SharedResources.getInstance().studies_extra_parameters_filename)
             # Patient unique ID might include characters
-            self.extra_patient_parameters.loc[:, 'Patient'] = self.extra_patient_parameters.Patient.astype(int).astype(
-                str)
-
+            try:
+                self.extra_patient_parameters.loc[:, 'Patient'] = self.extra_patient_parameters.Patient.astype(int).astype(str)
+            except Exception as e:
+                print(f"Convert patient IDs to int failed, error {e}")
+            self.extra_patient_parameters.loc[:, 'Patient'] = self.extra_patient_parameters.Patient.astype(str)
         self.optimal_overlap = None
         self.optimal_threshold = None
 
@@ -60,10 +70,11 @@ class HGGPostopTestsetInference:
         self.__generate_dice_scores()
         self.__retrieve_optimum_values()
         self.__read_results()
+        self.__drop_exclude_ids()
         self.__compute_and_plot_overall()
 
         if self.extra_patient_parameters is not None:
-            self.__compute_and_plot_metric_over_metric_categories(data=self.results, metric1='Dice', metric2='Volume', metric2_cutoffs=[1.])
+            self.__compute_and_plot_metric_over_metric_categories(data=self.results, metric1='Dice', metric2='True postop volume', metric2_cutoffs=[1.])
             volume_figure_fname = Path(self.output_folder, 'volume_cutoff.png')
             self.__compute_and_plot_volume_cutoff_results(volume_cutoff_range=[0., 0.5], optimal_cutoff=0.175, save_fname=volume_figure_fname)
             results_cutoff = self.__compute_results_cutoff_volume(cutoff_volume=0.175)
@@ -71,7 +82,8 @@ class HGGPostopTestsetInference:
             compute_fold_average(self.input_folder, results_cutoff, best_threshold=self.optimal_threshold,
                                  best_overlap=self.optimal_overlap,
                                  suffix='volume_cutoff', output_folder=self.output_folder)
-
+            results_cutoff = self.__compute_EOR(results_cutoff, crop_to_zero=True)
+            self.__study_volume_and_EOR(results_cutoff)
 
     def __retrieve_optimum_values(self):
         study_filename = os.path.join(self.input_folder, 'Validation', 'optimal_dice_study.csv')
@@ -100,6 +112,13 @@ class HGGPostopTestsetInference:
         except Exception as e:
             print('{}'.format(traceback.format_exc()))
 
+    def __drop_exclude_ids(self):
+        drop_index_res = self.results[self.results['Patient'].isin(self.exclude_ids)].index
+        self.results.drop(drop_index_res, axis=0, inplace=True)
+
+        drop_index_opt = self.optimal_results[self.optimal_results['Patient'].isin(self.exclude_ids)].index
+        self.optimal_results.drop(drop_index_opt, axis=0, inplace=True)
+
     def __compute_and_plot_overall(self):
         """
         Generate average results across all folds and per fold for all the computed metrics.
@@ -121,7 +140,7 @@ class HGGPostopTestsetInference:
             self.__compute_dice_confidence_intervals(data=results_df)
 
             if self.extra_patient_parameters is not None:
-                self.__compute_results_metric_over_metric(data=results_df, metric1='Dice', metric2='Volume', suffix='')
+                self.__compute_results_metric_over_metric(data=results_df, metric1='Dice', metric2='True postop volume', suffix='')
         except Exception as e:
             print('{}'.format(traceback.format_exc()))
 
@@ -253,7 +272,7 @@ class HGGPostopTestsetInference:
                         results[k].append(v)
 
                 output = f"Cutoff = {cutoff}, recall = {res['Recall']}, precision = {res['Precision']}, "\
-                         f"F1 = {res['F1']}, accuracy = {res['Accuracy']}, tnr = {res['True negative rate']}, "\
+                         f"F1 = {res['F1']}, accuracy = {res['Accuracy']}, tnr = {res['Specificity']}, "\
                          f"positive rate = {res['Positive rate']}, negative rate = {res['Negative rate']}"
                 print(output)
 
@@ -272,15 +291,15 @@ class HGGPostopTestsetInference:
 
     def __compute_results_cutoff_volume(self, cutoff_volume, cutoff_gt=True):
         data = deepcopy(self.optimal_results)
-        data.loc[(data['Volume segmentation'] <= cutoff_volume), 'Volume segmentation'] = 0.0
-        data.loc[(data['Volume segmentation'] == cutoff_volume), '#Det'] = 0
+        data.loc[(data['Predicted postop volume'] <= cutoff_volume), 'Predicted postop volume'] = 0.0
+        data.loc[(data['Predicted postop volume'] == cutoff_volume), '#Det'] = 0
 
         if cutoff_gt:
-            data.loc[(data['Volume'] <= cutoff_volume), 'Volume'] = 0.0
-            data.loc[(data['Volume'] == 0), '#GT'] = 0
+            data.loc[(data['True postop volume'] <= cutoff_volume), 'True postop volume'] = 0.0
+            data.loc[(data['True postop volume'] == 0), '#GT'] = 0
 
         # Drop columns missing volume
-        data.dropna(axis=0, subset=['Volume'], inplace=True)
+        data.dropna(axis=0, subset=['True postop volume'], inplace=True)
 
         return data
 
@@ -298,7 +317,7 @@ class HGGPostopTestsetInference:
         if not os.path.exists(self.dice_output_filename):
             self.results_df = pd.DataFrame(columns=['Fold', 'Patient', 'Threshold', 'Dice', 'Inst DICE', 'Inst Recall',
                                                     'Inst Precision', 'Largest foci Dice', '#GT', '#Det',
-                                                    'Volume segmentation'])
+                                                    'Predicted postop volume'])
         else:
             self.results_df = pd.read_csv(self.dice_output_filename)
             if self.results_df.columns[0] != 'Fold':
@@ -314,6 +333,7 @@ class HGGPostopTestsetInference:
         results_per_folds.append(results)
 
     def __generate_dice_scores_for_fold(self, data_list, fold_number):
+        data_list = [d.replace('\n', '') for d in data_list]
         for i, patient in enumerate(tqdm(data_list)):
             uid = None
             try:
@@ -321,7 +341,7 @@ class HGGPostopTestsetInference:
                 # start = time.time()
                 uid = patient.split('_')[1]
                 sub_folder_index = patient.split('_')[0]
-                patient_extended = '_'.join(patient.split('_')[1:-1]).strip()
+                patient_extended = '_'.join(patient.split('_')[1:]).strip().replace('_sample', '')
 
                 # Checking if values have already been computed for the current patient to skip it if so.
                 # In case values were not properly computed for the core part (i.e. first 10 columns without
@@ -401,3 +421,56 @@ class HGGPostopTestsetInference:
                 print(traceback.format_exc())
                 continue
         return 0
+
+    def __compute_EOR(self, results, crop_to_zero=False):
+        data = deepcopy(results)
+
+        true_EOR = np.array((results.loc[:, 'True preop volume'] - results.loc[:, 'True postop volume']) / results.loc[:, 'True preop volume'])
+        data['True EOR'] = true_EOR
+        #print(true_EOR[np.where(true_EOR<0)])
+        #print(data.loc[np.where(true_EOR < 0), 'Patient'].values)
+
+        predicted_EOR_type1 = np.array((results.loc[:, 'True preop volume'] - results.loc[:, 'Predicted postop volume']) / results.loc[:, 'True preop volume'])
+        data['Predicted EOR type 1'] = predicted_EOR_type1
+        #print(predicted_EOR_type1[np.where(predicted_EOR_type1 < 0)])
+        #print(data.loc[np.where(predicted_EOR_type1 < 0), 'Patient'].values)
+
+        if crop_to_zero:
+            data.loc[(data['True EOR'] < 0), 'True EOR'] = 0.0
+            data.loc[(data['Predicted EOR type 1'] < 0), 'Predicted EOR type 1'] = 0.0
+
+        return data
+
+    def __study_volume_and_EOR(self, results):
+        data = deepcopy(results)
+        output_folder = Path(self.output_folder, 'Volume-EOR')
+        output_folder.mkdir(exist_ok=True)
+        sns.set_style('ticks')
+
+        # EOR
+        save_fname = str(Path(output_folder, f'EOR_scatter_{self.study_name}_Test.png'))
+        plt.figure()
+        ax = sns.scatterplot(data=data*100, x='True EOR', y='Predicted EOR type 1')
+        ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c=".1")
+        ax.set(title=f'True vs predicted EOR', xlabel='True EOR (%)', ylabel='Predicted EOR (%)')
+        plt.savefig(save_fname)
+
+        save_fname = str(Path(output_folder, f'EOR_bland_altman_{self.study_name}_Test.png'))
+        blandAltman(data['True EOR'], data['Predicted EOR type 1'],
+                    title=f'Bland-Altman of true vs predicted EOR', savePath=save_fname)
+
+        # VOLUME
+        save_fname = str(Path(output_folder, f'volume_scatter_{self.study_name}_Test.png'))
+        plt.figure()
+        plt.xscale('symlog')
+        plt.yscale('symlog')
+        ax = sns.scatterplot(data=data, x='True postop volume', y='Predicted postop volume')
+        ax.plot(ax.get_xlim(), ax.get_ylim(), ls="--", c=".1")
+        #ax.title('True vs predicted postop volume (symlog scale)')
+        ax.set(title=f'True vs predicted postop volume (symlog scale)',
+               xlabel='True postop volume (ml)', ylabel='Predicted postop volume (ml)')
+        plt.savefig(save_fname)
+
+        save_fname = str(Path(output_folder, f'volume_bland_altman_{self.study_name}_Test.png'))
+        blandAltman(data['True postop volume'], data['Predicted postop volume'],
+                    title=f'Bland-Altman of true vs predicted postop volume', savePath=save_fname)

Validation/validation_utilities.py
@@ -304,7 +304,7 @@ def compute_fold_average(folder, data=None, best_threshold=0.5, best_overlap=0.0
     fold_average_columns = ['Fold', '# samples', 'Patient-wise recall', 'Patient-wise precision', 'Patient-wise F1',
                             'FPPP', 'Object-wise recall', 'Object-wise precision', 'Object-wise F1',
                             'Patient-wise recall postop', 'Patient-wise precision postop', 'Patient-wise F1 postop',
-                            'Accuracy', 'Balanced accuracy', 'Positive rate', 'Negative rate', 'TNR']
+                            'Accuracy', 'Balanced accuracy', 'Positive rate', 'Negative rate', 'Specificity']
     for m in metric_names:
         if m in results.columns.values or 'Dice' in m:
             fold_average_columns.extend([m + '_mean', m + '_std'])
@@ -346,13 +346,13 @@ def compute_fold_average(folder, data=None, best_threshold=0.5, best_overlap=0.0
         patient_wise_precision_postop = true_positives / (true_positives + false_positives)
         patient_wise_F1_postop = 2 * ((patient_wise_recall_postop * patient_wise_precision_postop) / (patient_wise_precision_postop + patient_wise_recall_postop))
         accuracy = (true_positives + true_negatives ) / (true_negatives + true_positives + false_negatives + false_positives)
-        true_negative_rate = 1 if (true_negatives + false_negatives) == 0 else true_negatives / (true_negatives + false_negatives)
-        balanced_accuracy = (patient_wise_recall_postop + true_negative_rate) / 2
+        specificity = 1 if (true_negatives + false_positives) == 0 else true_negatives / (true_negatives + false_positives)
+        balanced_accuracy = (patient_wise_recall_postop + specificity) / 2
 
         fold_average = [f, len(np.unique(fold_results['Patient'].values)), patient_wise_recall, patient_wise_precision,
                         patient_wise_F1, fppp, object_wise_recall, object_wise_precision, object_wise_F1,
                         patient_wise_recall_postop, patient_wise_precision_postop, patient_wise_F1_postop, accuracy, balanced_accuracy,
-                        (true_positives + false_negatives)/len(all_for_thresh), (false_positives + true_negatives)/len(all_for_thresh), true_negative_rate]
+                        (true_positives + false_negatives)/len(all_for_thresh), (false_positives + true_negatives)/len(all_for_thresh), specificity]
         for m in metric_names:
             if m in fold_results.columns.values:
                 if m in ['HD95', 'ASSD', 'RAVD', 'VC', 'OASSD']:
@@ -394,7 +394,8 @@ def compute_fold_average(folder, data=None, best_threshold=0.5, best_overlap=0.0
     fold_averaged_results = [total_samples]
     fixed_metrics = ['Patient-wise recall', 'Patient-wise precision', 'Patient-wise F1', 'FPPP', 'Object-wise recall',
                      'Object-wise precision', 'Object-wise F1', 'Patient-wise recall postop', 'Patient-wise precision postop',
-                     'Patient-wise F1 postop', 'Accuracy', 'Balanced accuracy', 'Positive rate', 'Negative rate', 'TNR']
+                     'Patient-wise F1 postop', 'Accuracy', 'Balanced accuracy', 'Positive rate', 'Negative rate', 'Specificity']
+
     if dice_fixed_metric:
         fixed_metrics = fixed_metrics + ['Dice_mean', 'Dice-TP_mean', 'Dice-P_mean', 'Dice-N_mean']
 