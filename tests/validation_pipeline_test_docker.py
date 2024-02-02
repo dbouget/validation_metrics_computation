263a24047d977f0ec707ec789939bfa4e2375b8b
dbouget <bougetd@gmail.com>
2024-02-02 09:12:47 +0000

    Starting to include Docker support [skip ci]

--- tests/validation_pipeline_test_docker.py
+++ tests/validation_pipeline_test_docker.py
@@ -15,10 +15,18 @@ except:
     import requests
 
 
-def validation_pipeline_test():
+def inference_test_docker():
+    """
+    Testing the CLI within a Docker container for the validation unit test, running on CPU.
+    The latest Docker image is being hosted at: dbouget/raidionics-val:v1.0-py38-cpu
+
+    Returns
+    -------
+
+    """
     logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)
-    logging.info("Running standard reporting unit test.\n")
+    logging.info("Running validation unit test in Docker container.\n")
     logging.info("Downloading unit test resources.\n")
     test_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'unit_tests_results_dir')
     if os.path.exists(test_dir):
@@ -51,12 +59,12 @@ def validation_pipeline_test():
     try:
         val_config = configparser.ConfigParser()
         val_config.add_section('Default')
-        val_config.set('Default', 'task', 'validation')
-        val_config.set('Default', 'data_root', os.path.join(test_dir, 'Input_dataset'))
-        val_config.set('Default', 'number_processes', "1")
+        val_config.set('Default', 'task', "validation")
+        val_config.set('Default', 'data_root', '/workspace/resources/Input_dataset')
+        val_config.set('Default', 'number_processes', '1')
         val_config.add_section('Validation')
-        val_config.set('Validation', 'input_folder', os.path.join(test_dir, 'StudyResults'))
-        val_config.set('Validation', 'output_folder', os.path.join(test_dir, 'StudyResults'))
+        val_config.set('Validation', 'input_folder', '/workspace/resources/StudyResults')
+        val_config.set('Validation', 'output_folder', '/workspace/resources/StudyResults')
         val_config.set('Validation', 'gt_files_suffix', 'label_tumor.nii.gz')
         val_config.set('Validation', 'prediction_files_suffix', 'pred_tumor.nii.gz')
         val_config.set('Validation', 'use_index_naming_convention', 'false')
@@ -69,60 +77,44 @@ def validation_pipeline_test():
         val_config.set('Validation', 'tiny_objects_removal_threshold', '25')
         val_config.set('Validation', 'true_positive_volume_thresholds', '0.1')
         val_config.set('Validation', 'use_brats_data', 'false')
-        config_filename = os.path.join(test_dir, 'config.ini')
-        with open(config_filename, 'w') as outfile:
+        val_config_filename = os.path.join(test_dir, 'test_val_config.ini')
+        with open(val_config_filename, 'w') as outfile:
             val_config.write(outfile)
 
-        logging.info("Running k-fold cross-validation unit test.\n")
-        from raidionicsval.compute import compute
-        compute(config_filename)
-
-        logging.info("Collecting and comparing results.\n")
-        scores_filename = os.path.join(test_dir, 'StudyResults', 'Validation', 'all_dice_scores.csv')
-        if not os.path.exists(scores_filename):
-            logging.error("k-fold cross-validation unit test failed, no scores were generated.\n")
-            if os.path.exists(test_dir):
-                shutil.rmtree(test_dir)
-            raise ValueError("k-fold cross-validation unit test failed, no scores were generated.\n")
-
-        logging.info("k-fold cross-validation CLI unit test started.\n")
+        logging.info("Running validation unit test in Docker container.\n")
         try:
             import platform
+            cmd_docker = ['docker', 'run', '-v', '{}:/workspace/resources'.format(test_dir),
+                          '--network=host', '--ipc=host', 'dbouget/raidionics-val:v1.0-py38-cpu',
+                          '-c', '/workspace/resources/test_val_config.ini', '-v', 'debug']
+            logging.info("Executing the following Docker call: {}".format(cmd_docker))
             if platform.system() == 'Windows':
-                subprocess.check_call(['raidionicsval',
-                                       '{config}'.format(config=config_filename),
-                                       '--verbose', 'debug'], shell=True)
-            elif platform.system() == 'Darwin' and platform.processor() == 'arm':
-                subprocess.check_call(['python3', '-m', 'raidionicsval',
-                                       '{config}'.format(config=config_filename),
-                                       '--verbose', 'debug'])
+                subprocess.check_call(cmd_docker, shell=True)
             else:
-                subprocess.check_call(['raidionicsval',
-                                       '{config}'.format(config=config_filename),
-                                       '--verbose', 'debug'])
+                subprocess.check_call(cmd_docker)
         except Exception as e:
-            logging.error("Error during k-fold cross-validation CLI unit test with: \n {}.\n".format(traceback.format_exc()))
+            logging.error("Error during validation test in Docker container with: \n {}.\n".format(traceback.format_exc()))
             if os.path.exists(test_dir):
                 shutil.rmtree(test_dir)
-            raise ValueError("Error during k-fold cross-validation CLI unit test.\n")
+            raise ValueError("Error during validation test in Docker container.\n")
 
         logging.info("Collecting and comparing results.\n")
-        scores_filename = os.path.join(test_dir, 'StudyResults', 'Validation', 'all_dice_scores.csv')
-        if not os.path.exists(scores_filename):
-            logging.error("k-fold cross-validation CLI unit test failed, no scores were generated.\n")
+        brain_segmentation_filename = os.path.join(test_dir, 'StudyResults', 'Validation', 'all_dice_scores.csv')
+        if not os.path.exists(brain_segmentation_filename):
+            logging.error("Validation in Docker container failed, no dice scores were generated.\n")
             if os.path.exists(test_dir):
                 shutil.rmtree(test_dir)
-            raise ValueError("k-fold cross-validation CLI unit test failed, no scores were generated.\n")
-        logging.info("k-fold cross-validation CLI unit test succeeded.\n")
+            raise ValueError("Validation in Docker container failed, no dice scores were generated.\n")
+        logging.info("Validation unit test in Docker container succeeded.\n")
     except Exception as e:
-        logging.error("Error during k-fold cross-validation unit test with: \n {}.\n".format(traceback.format_exc()))
+        logging.error("Error during validation in Docker container with: \n {}.\n".format(traceback.format_exc()))
         if os.path.exists(test_dir):
             shutil.rmtree(test_dir)
-        raise ValueError("Error during k-fold cross-validation unit test with.\n")
+        raise ValueError("Error during validation in Docker container with.\n")
 
-    logging.info("k-fold cross-validation unit test succeeded.\n")
+    logging.info("Validation unit test in Docker container succeeded.\n")
     if os.path.exists(test_dir):
         shutil.rmtree(test_dir)
 
 
-validation_pipeline_test()
+inference_test_docker()
